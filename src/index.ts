c9dfa4fca764b33e7ef9747c0eaf4719789cb3ae
Leonardo Souza <leonardomellolopes@gmail.com>
2024-11-14 21:50:38 -0300

    ultimo commit da noite

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-import { questionFloat } from 'readline-sync'
+import { questionInt } from 'readline-sync'
 
 //Calcule a media de 4 notas e entregue a saida com a media entre as notas
 
-const nota1 = questionFloat("Informe a primeira nota:");
-const nota2 = questionFloat("Informe a segunda nota:");;
-const nota3 = questionFloat("Informe a terceira nota:");;
-const nota4 = questionFloat("Informe a quarta nota:");;
+//array ou vetor
+let notas: [number, number, number, number, number] = [0,0,0,0,0];
+
+for(let i = 0; i < 5; i++){
+    const nota = questionInt("Informe a nota "+(i+1)+":");
+    notas[i] = nota;
+
+}
+
+const nota1 = questionInt("Informe a primeira nota: ");
+const nota2 = questionInt("Informe a segunda nota: ");;
+const nota3 = questionInt("Informe a terceira nota: ");;
+const nota4 = questionInt("Informe a quarta nota: ");;
 
 //PROCESSAMENTO
 const media = (nota1+nota2+nota3+nota4)/4;
 
 if(media >=7) {
-    console.log("Aluno Aprovado");
+    console.log("Aluno Aprovado: " + media);
 }else {
-    console.log("Aluno Reprovado");
+    console.log("Aluno Reprovado: " + media);
 }
